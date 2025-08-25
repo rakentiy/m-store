e3a19d554848cacccba5acc872797a1f53c0a480
Ravshan Makharov <rakentiy@gmail.com>
2025-08-25 16:37:17 +0500

    Created install command

---
+++ app/Console/Commands/InstallCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    protected $signature = 'shop:install';
+
+    protected $description = 'Shop Installation';
+
+    public function handle()
+    {
+        $this->call('storage:link');
+        $this->call('migrate');
+        return self::SUCCESS;
+    }
+}
